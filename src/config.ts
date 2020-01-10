39bb5d73b7e0e8846794a25a7502a0a971d5861d
myx <john@smith.kyon>
2020-01-10 14:02:02 +0800

    fix: 修改 IO 目录

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,16 @@
 import path = require('path');
+import fs = require('fs');
 
 const smsFileName = '3d0d7e5fb2ce288813306e4d4636395e047a3d28';
 const contactsFileName = '31bb7ba8914766d4ba40d6dfb6113c8b614be442';
-const outputFileName = 'result';
+
+const packageJson = fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf8');
+const outputFileName = JSON.parse(packageJson).name;
 
 export const rootPath = path.resolve(__dirname, '../');
-export const dataSetPath = path.resolve(rootPath, 'dataset');
-export const smsPath = path.resolve(dataSetPath, smsFileName);
-export const contactsPath = path.resolve(dataSetPath, contactsFileName);
-export const outputExcelPath = path.resolve(rootPath, 'output', `${outputFileName}.csv`);
-export const outputJsonPath = path.resolve(rootPath, 'output', `${outputFileName}.json`);
+export const smsPath = path.resolve(rootPath, smsFileName);
+export const contactsPath = path.resolve(rootPath, contactsFileName);
+export const outputExcelPath = path.resolve(rootPath, `${outputFileName}.csv`);
+export const outputJsonPath = path.resolve(rootPath, `${outputFileName}.json`);
 
 export const mapHandleContactFileName = 'map-handle-contact';
